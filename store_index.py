b719eadfe0551f67493f46ee2d533efc089a8687
Charan <mcharan1996@gmail.com>
2025-05-27 17:41:17 -0700

    Added chatbot functionalties in jupyter notebook and later modularized into multiple files.

---
+++ store_index.py
@@ -0,0 +1,28 @@
+# If we want to update the book or add/remove information, we again need to store all 
+# the embeddings in the pincoce. So whenever this store_index.py if executed, it will load 
+# the new information and store it in the vector database.
+
+from src.helper import load_pdf_file, text_split, download_hugging_face_embeddings
+from pinecone.grpc import PineconeGRPC as Pinecone
+from pinecone import ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+
+
+extracted_data=load_pdf_file(data='Data/')
+text_chunks=text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+
+# Embed each chunk and upsert the embeddings into your Pinecone index.
+docsearch = PineconeVectorStore.from_documents(
+    documents=text_chunks,
+    index_name=index_name,
+    embedding=embeddings, 
+)
